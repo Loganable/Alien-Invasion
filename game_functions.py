dd686534bee8bb9ad11e541d3c24cdf8228361e0
Logan <40294656+Loganable@users.noreply.github.com>
2019-05-04 11:54:13 +0800

    Add files via upload
    优化：优化代码
    新增：结束游戏

--- game_functions.py
+++ game_functions.py
@@ -61,9 +61,11 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    
-    # 检查是否有子弹击中了外星人
-    # 如果是这样，就删除相应的子弹和外星人
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """响应子弹和外星人的碰撞"""
+    # 删除发生碰撞的子弹和外星人
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     
     if len(aliens) == 0:
@@ -119,10 +121,14 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def update_aliens(ai_settings, aliens):
+def update_aliens(ai_settings, ship, aliens):
     """
-    检查是否有外星人位于屏幕边缘，并更新整群外星人的位置
+    检查是否有外星人位于屏幕边缘，然后更新所有外星人的位置
     """
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
+    
+    # 检测外星人与飞船之间的碰撞
+    if pygame.sprite.spritecollideany(ship, aliens):
+        print("BOOM!!!")
         
